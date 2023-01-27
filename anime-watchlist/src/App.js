7a38a29ca2fa946cbc33b10fc42bb88e7b15293e
Elias AJEDDIG <eajeddig@gmail.com>
2023-01-28 01:22:49 +0900

    Search Function + Anime Card creation

--- anime-watchlist/src/App.js
+++ anime-watchlist/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import './Components/style.css'
 function App() {
+  
+  const [animeData,setAnimeData]=useState();
+  const [search, setSearch]=useState('One Piece');
+
+  const getData=async()=>{
+    const res=await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=20`)
+    const resData= await res.json();
+    //console.log(res);
+    //console.log(resData);
+    setAnimeData(resData.data)
+}
+
+  
+  useEffect(()=>{
+    getData()
+  },[search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="header">
+      <h1>My Anime WatchList</h1>
+      <div className="search-box">
+        <input type="search" placeholder="Search your anime"
+        onChange={(e)=>setSearch(e.target.value)}/>
+      </div>
     </div>
+    </>
   );
 }
 
