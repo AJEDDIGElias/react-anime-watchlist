8a50df102aed8aa6d6ad030ab00e29ece5078293
Elias AJEDDIG <eajeddig@gmail.com>
2023-01-28 01:42:06 +0900

    Anime Cardlist finished + Starting Anime Info

--- anime-watchlist/src/App.js
+++ anime-watchlist/src/App.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import './Components/style.css'
+import { AnimeList } from "./Components/AnimeList";
+import { AnimeInfo } from "./Components/AnimeInfo";
 function App() {
   
   const [animeData,setAnimeData]=useState();
   const [search, setSearch]=useState('One Piece');
+  const [animeInfo, setAnimeInfo]=useState();
 
   const getData=async()=>{
     const res=await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=20`)
@@ -25,6 +28,21 @@ function App() {
       <div className="search-box">
         <input type="search" placeholder="Search your anime"
         onChange={(e)=>setSearch(e.target.value)}/>
+      </div>
+    </div>
+
+    <div className="container">
+      <div className="animeInfo">
+        {animeInfo && <AnimeInfo animeInfo={animeInfo}/>}
+      </div>
+      <div className="anime-row">
+        <h2 className="text-heading">Anime</h2>
+        <div className="row">
+          <AnimeList 
+          animeList={animeData}
+          setAnimeInfo={setAnimeInfo}
+          />
+         </div>
       </div>
     </div>
     </>
--- anime-watchlist/src/Components/AnimeList.js
+++ anime-watchlist/src/Components/AnimeList.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
-export const AnimeList = () => {
+export const AnimeList = ({animeList, setAnimeInfo}) => {
   return (
     <>
+    {
+        animeList ?(
+            animeList.map((anime,index) => {
+                return(
+                    <div className='card' key={index} onClick={()=>setAnimeInfo(anime)} >
+                        <img src={anime.images.jpg.large_image_url} alt="animeImage" />
+                        <div className='anime-Info'>
+                            <h4>{anime.title}</h4>
+                        </div>
+                    </div>
+                )
+            })
+        ) : "Not Found"
+    }
     
     </>
   )
