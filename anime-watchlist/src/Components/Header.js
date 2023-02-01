779fcbdc17466c33d81e4e511dc6b97d267cd88b
Elias AJEDDIG <eajeddig@gmail.com>
2023-02-02 04:55:12 +0900

    Add Gif for README

---
+++ anime-watchlist/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+/**const Header = () => {
+    return (
+        <div className="header">
+            <h1>MyAnimeList</h1>
+            <div className="search-box">
+                <input type="search" placeholder="Search your anime" 
+            onChange={(e)=>setSearch(e.target.value)}/>
+            </div>
+        </div>
+    )
+}*/
